11481baaaa05314163a794e3a3fdc38cd4d4d213
Terrence Nixa <tnixa@us.ibm.com>
2019-04-19 12:32:32 -0500

    updated pom and changed exception handling

--- src/main/java/org/terrence/testapp/rest/TestRestController.java
+++ src/main/java/org/terrence/testapp/rest/TestRestController.java
@@ -1,9 +1,11 @@
 package org.terrence.testapp.rest;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.terrence.testapp.domain.Person;
 import org.terrence.testapp.repositories.PersonRepository;
@@ -14,43 +16,87 @@ public class TestRestController {
   @Autowired
   private PersonRepository repo;
 
-  @GetMapping("/test")
+  StringWriter sw = new StringWriter();
+  PrintWriter pw = new PrintWriter(sw);
 
-  public String runTest() {
+  Person test = new Person("TestPerson", 33); // test person object with name and age
+  String id = UUID.randomUUID().toString(); // use a random id
+
+  // test methods
+
+  // create test object with random id and fixed name and age
+  private void createTestObject() throws Exception {
     try {
-      System.out.println("this is just a test");
-      Person test = new Person("TestPerson", 33);
-      String id = UUID.randomUUID().toString(); // use a random id
-      System.out.println("Using random repo id: " + id);
+      pw.println("Name of the test object is: '" + test.getName() + "'");
+      pw.println("Age of the test objec is: '" + test.getAge() + "'");
+      pw.println("Random repo id of the test object is: '" + id + "'");
+      pw.println("Setting id for test object: '" + id + "'");
       test.setId(id);
+    } catch (Exception e) {
+      pw.println("FAIL: Unexpected error during create test object.");
+      e.printStackTrace(pw);
+      throw e;
+    }
+  }
 
-      // verify there is nothing in the repo with the id and then create the test
-      // object
-      try {
-        repo.findById(id).ifPresent(p -> repo.deleteById(p.getId())); // if there is an existing Person with the id then
-                                                                      // delete it
-        repo.insert(test);
-      } catch (Exception d) {
-        System.out.println("exception caught: creating new object");
-        repo.insert(test);
-      }
+  // verify there is nothing in the repo with the id and then create the test
+  // object
+  private void addTestObject() throws Exception {
+    try {
+      pw.println("Testing for existing id in the repo '" + id + "' and  deleting as required.");
+      // if there is an existing Person with the id then delete it
+      repo.findById(id).ifPresent(p -> repo.deleteById(p.getId()));
+      pw.println("Saving test object with id: '" + id + "'");
+      repo.insert(test);
+    } catch (Exception e) {
+      System.out.println("Exception caught: creating new object");
+      pw.println("Saving test object with id: '" + id + "'");
+      repo.insert(test);
+      throw e;
+    }
+  }
 
-      // get the Person by ID and make sure the name and id matche
+  // validate the the test object is in the database with the correct id and
+  // message
+  private void validateTestObject() throws Exception {
+    try {
+      // retrieve check object from the repo with the same id as the test object
       Person check = repo.findById(id).get();
+
+      // validate the id and message of the check object equals the test object's
+      pw.println("Validating object in the repo.");
       if (((check.getId() == null && test.getId() == null)
           || (check.getId() != null && check.getId().equals(test.getId())))
           && ((check.getName() == null && test.getName() == null)
               || (check.getName() != null && check.getName().equals(test.getName())))) {
+        pw.println("PASS: Retrieved object in the repo matced the saved object.");
+        pw.println("Deleting object in the repo.");
         repo.deleteById(id);
-        return "test passed: objects matched!";
       } else {
+        pw.println("FAIL: Retrieved object in the repo did NOT match the saved object");
+        pw.println("Deleting object in the repo.");
         repo.deleteById(id);
-        return "test failed: ojects do not match";
       }
     } catch (Exception e) {
-      e.printStackTrace();
-      System.out.println(e);
+      pw.println("FAIL: Problem during retrieve of object...");
+      e.printStackTrace(pw);
+      throw e;
     }
-    return "done";
+  }
+
+  // run the test
+  @RequestMapping(value = "/test", produces = "text/plain")
+  public String runTest() {
+    try {
+      pw.println("Beginning test...");
+      createTestObject();
+      addTestObject();
+      validateTestObject();
+
+    } catch (Exception e) {
+      pw.println("Failure reported by previous method.");
+    }
+    pw.flush();
+    return sw.toString();
   }
 }
